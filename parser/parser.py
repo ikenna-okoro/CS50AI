8982ef22e94587ef53ac120857197286ba1d12a2
ikenna <ike_genius1@yahoo.com>
2026-03-01 03:19:36 +0000

    Project implementation for parser was performed

--- parser/parser.py
+++ parser/parser.py
@@ -1,5 +1,8 @@
 import nltk
+from nltk.tree import Tree
 import sys
+import re
+from nltk.tokenize import word_tokenize
 
 TERMINALS = """
 Adj -> "country" | "dreadful" | "enigmatical" | "little" | "moist" | "red"
@@ -15,7 +18,18 @@ V -> "smiled" | "tell" | "were"
 """
 
 NONTERMINALS = """
-S -> N V
+S -> NP VP
+S -> NP VP Conj NP VP
+NP -> N | Det N 
+NP -> Det Adj N | Det Adj Adj N | Det Adj Adj Adj N 
+NP -> NP PP  
+NP -> NP Conj NP
+VP -> V | V NP
+VP -> VP PP
+VP -> VP PP NP
+VP -> VP Adv | Adv VP
+VP -> VP Conj VP
+PP -> P | P NP
 """
 
 grammar = nltk.CFG.fromstring(NONTERMINALS + TERMINALS)
@@ -62,8 +76,15 @@ def preprocess(sentence):
     and removing any word that does not contain at least one alphabetic
     character.
     """
-    raise NotImplementedError
+    token_bank = []
 
+    # s = word_tokenize(sentence)
+    s = re.split(r"[^\w]+", sentence)
+    for t in s:
+        if any(char.isalpha() for char in t):
+            token_bank.append(t.lower())
+
+    return token_bank
 
 def np_chunk(tree):
     """
@@ -72,7 +93,31 @@ def np_chunk(tree):
     whose label is "NP" that does not itself contain any other
     noun phrases as subtrees.
     """
-    raise NotImplementedError
+    chunk = []
+
+    def traverse(node):
+        if not isinstance(node, Tree):
+            return
+        
+        # Base case
+        if node.label() == "NP" and not contains_np(node):
+            chunk.append(node)
+            return
+        
+        # Recursive case
+        for child in node:
+            traverse(child)
+
+    traverse(tree)
+    return chunk
+
+def contains_np(tree):
+    return any(
+        subtree.label() == "NP" 
+        for subtree in tree.subtrees() 
+        if subtree != tree
+        )
+
 
 
 if __name__ == "__main__":
